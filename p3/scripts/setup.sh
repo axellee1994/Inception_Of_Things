49e671320c08dcf83d0bdc8549e03e1f9d4c42d2
ssian <ssian@ssian.42singapore.sg>
2026-02-11 18:47:23 +0800

    fix Vagrant file and setup.sh

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -8,7 +8,6 @@ kubectl wait --for=condition=Ready nodes --all --timeout=120s
 kubectl config use-context k3d-mycluster
 
 kubectl apply --server-side -f ../confs/namespace.yaml
-#kubectl apply -f ../confs/namespace.yaml
 
 kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
